b74d593d8f89648f03b525ae069b42f32af7bffa
Hetva mehta <hetvamehta01@gmail.com>
2026-01-13 13:54:38 +0530

    Java program demonstrating the use of command-line arguments to accept two numbers.

---
+++ Week-01/SumOfTwoNumbers.java
@@ -0,0 +1,16 @@
+class SumOfTwoNumbers {
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("Please provide exactly two numbers as command-line arguments.");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
+    }
+}
