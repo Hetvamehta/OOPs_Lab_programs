3aea76559839864118a67e70e5881a92cc2f0086
Hetva mehta <hetvamehta01@gmail.com>
2026-02-19 11:04:34 +0530

    Implement University class with student counter and static/instance initialization blocks.

---
+++ Week-03/University.java
@@ -0,0 +1,35 @@
+public class University {
+    static int totalStudents = 0;
+    static String universityName;
+
+    static {
+        universityName = "Global Tech University";
+        System.out.println("Static Block: University name initialized to " + universityName);
+    }
+
+    {
+        totalStudents++;
+        System.out.println("Instance Block: A new student object is being created.");
+    }
+
+    public University() {
+        System.out.println("Constructor: Student successfully enrolled.");
+    }
+
+    public static int getTotalStudents() {
+        return totalStudents;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main Method: Starting execution...");
+        System.out.println("Welcome to " + University.universityName);
+        
+        System.out.println("\n--- Creating Student 1 ---");
+        new University();
+        
+        System.out.println("\n--- Creating Student 2 ---");
+        new University();
+        
+        System.out.println("\nTotal Students Enrolled: " + University.getTotalStudents());
+    }
+}
